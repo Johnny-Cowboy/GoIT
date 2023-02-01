b5e97d1338f27e7e57fca914c1a524b9bd9904d5
JohnCowboy <dawid.kowalski@protonmail.com>
2023-02-01 18:03:04 +0100

    zajęcia 3

--- script.js
+++ script.js
@@ -8,19 +8,36 @@ function myColorChange() {
   element.style.color = "red";
 }
 
-function mojaFunkcja(imie) {
-  const clients = ["Mango", "Poly", "Ajax"];
-  const clientNameToFind = "Poly";
-  let message;
+const przesylka = {
+  name: "",
+  ulica: "",
+};
 
-  for (const client of clients) {
-    if (client === imie) {
-      message = "Klient z takim imieniem jest w bazie danych!";
-      break;
-    }
+const przesylka2 = {
+  name: "",
+  ulica: "",
+};
 
-    message = "Nie znaleźliśmy takiego klienta w bazie danych!";
+przesylka.name = "List";
+przesylka2.name = "Paczka";
+przesylka2.waga = 20;
+
+function mojaFunkcja(id) {
+  const element = document.getElementById("id01");
+  const element2 = document.getElementById("id02");
+  if (id === 1) {
+    //Informacja dla przycisku z informacja o przesylce 1
+    informacja = "Typ Twojej przesyłki to: " + przesylka.name;
+    element.innerHTML = informacja;
+
+    informacja = "Presyłka nie posiada wagi";
+    element2.innerHTML = informacja;
+  } else if (id === 2) {
+    //Informacja dla przycisku z informacja o przesylce 2
+    informacja = "Typ Twojej przesyłki to: " + przesylka2.name;
+    element.innerHTML = informacja;
+
+    informacja = "Waga Twojej przesyłki to: " + przesylka2.waga + " kg";
+    element2.innerHTML = informacja;
   }
-
-  console.log(message); //
 }
